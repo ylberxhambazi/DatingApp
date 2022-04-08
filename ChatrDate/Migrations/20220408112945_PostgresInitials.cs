efc415edc3b386ba8ed3601c694e72154e3fc3e7
Ylber Xhambazi <ylber.xhambazi@ndbit.net>
2022-04-08 14:43:49 +0200

    js files

---
+++ ChatrDate/Migrations/20220408112945_PostgresInitials.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatrDate.Migrations
+{
+    public partial class PostgresInitials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
